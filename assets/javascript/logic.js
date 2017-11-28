1e4134c274e857323d95462689a0ca241153bb80
willhoy61 <willhoy61@gmail.com>
2017-11-28 16:30:25 -0600

    sends data to firebase but not back to doc

---
+++ assets/javascript/logic.js
@@ -0,0 +1,54 @@
+ var config = {
+    apiKey: "AIzaSyCr8rv4DOWN_L5oJoaEMHRysAXyCTj6mXI",
+    authDomain: "trainstation-6a9c0.firebaseapp.com",
+    databaseURL: "https://trainstation-6a9c0.firebaseio.com",
+    projectId: "trainstation-6a9c0",
+    storageBucket: "",
+    messagingSenderId: "201938784073"
+  };
+  
+  firebase.initializeApp(config);
+
+ var database = firebase.database();
+
+$("#add-train-btn").on("click", function(event){
+	event.preventDefault();
+
+var train = $("#trainName").val().trim();
+var dest = $("#destination").val().trim();
+var fTrain = $("#firstTrainTime").val().trim();
+var frequent = $("#frequency").val().trim();
+
+var newTrain = {
+	trainName : train,
+	destination : dest,
+	firstTrain : fTrain,
+	frequency : frequent,
+};
+
+database.ref().push(newTrain);
+
+console.log(newTrain.trainName);
+console.log(newTrain.destination);
+console.log(newTrain.firstTrain);
+console.log(newTrain.frequency);
+
+$("#trainName").val("");
+$("destination").val("");
+$("#firstTrainTime").val("");
+$("frequency").val("");
+
+});
+
+database.ref().on("child_added", function(childSnapShot, prevChildKey){
+
+
+
+var train = childSnapshot.val().trainName;
+var dest = childSnapshot.val().destination;
+var fTrain = childSnapshot.val().firstTrain;
+var frequent = childSnapshot.val().frequency;
+
+$("#trainTable > tbody").append("<tr><td>" + train + "</td><td>" + dest + "</td><td>" + fTrain + "</td><td>" + frequent + "</td></tr>");
+})
+
